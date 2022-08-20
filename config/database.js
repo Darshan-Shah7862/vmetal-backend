ba0fcf6fbc72b7a93367abc046c043bff8da14f9
Darshan-Shah7862 <darshanshah7862@gmail.com>
2022-08-20 05:40:28 +0530

    Added

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
-const path = require('path');
-
+const parse = require("pg-connection-string").parse;
+const config = parse(
+  "postgres://snbhqhhpngfvqd:b97dddb7a80d324ce3d9580e2f1486b1a965b37ab03ffcf74d58465451e594ae@ec2-54-86-106-48.compute-1.amazonaws.com:5432/dduhlr77ds7eqk"
+);
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
