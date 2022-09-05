c0af1bf7bd30d19a643ac1c3197be54bf4542e99
Darshan-appyhigh <98078315+Darshan-appyhigh@users.noreply.github.com>
2022-09-05 10:56:55 +0530

    Updated: server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,1 +1,9 @@
-module.exports = ({ env }) => ({ url: env("MY_HEROKU_URL") });
+module.exports = ({ env }) => ({
+  url: env("MY_HEROKU_URL"),
+
+  proxy: true,
+
+  app: {
+    keys: env("APP_KEYS"),
+  },
+});
